359976122ea1d5c80af25ba5759d5e04f988e3c1
opefago <damilola.fagoyinbo@gmail.com>
2026-03-08 23:24:49 -0700

    FIXED INNER RADIUS OF RING

--- src/labs/design-maker/behaviors/ObjectBehaviorFactory.js
+++ src/labs/design-maker/behaviors/ObjectBehaviorFactory.js
@@ -4,10 +4,10 @@ import TextBehavior from "./TextBehavior";
 import TorusBehavior from "./TorusBehavior";
 import RingBehavior from "./RingBehavior";
 
-const defaultBehavior = new DefaultShapeBehavior();
-const textBehavior = new TextBehavior();
-const torusBehavior = new TorusBehavior();
-const ringBehavior = new RingBehavior();
+const defaultBehavior  = new DefaultShapeBehavior();
+const textBehavior     = new TextBehavior();
+const torusBehavior    = new TorusBehavior();
+const ringBehavior     = new RingBehavior();
 const importedBehavior = new ImportedBehavior();
 
 export function getObjectBehavior(type) {
--- src/labs/design-maker/behaviors/RingBehavior.js
+++ src/labs/design-maker/behaviors/RingBehavior.js
@@ -4,19 +4,19 @@ export default class RingBehavior extends DefaultShapeBehavior {
   getScaleHandles(obj, objectDims = null) {
     const g = obj.geometry;
     const o = this.handleOffset;
-    // objectDims is world-space (from getObjectWorldDims), so for a flat ring:
-    //   hw = hd = outerRadius,  hh = height/2
     const hw = (objectDims?.width  ?? g.outerRadius * 2) / 2;
     const hh = (objectDims?.height ?? g.height)         / 2;
     const hd = (objectDims?.depth  ?? g.outerRadius * 2) / 2;
-    const innerR = g.innerRadius ?? g.outerRadius * 0.5;
+    // innerR scaled by X scale so the handle tracks the actual world inner rim
+    const scaleX = obj.scale?.[0] ?? 1;
+    const innerR = (g.innerRadius ?? g.outerRadius * 0.5) * scaleX;
 
     return [
-      { param: "outerRadius", dir: [1,  0, 0], pos: [hw + o, -hh, 0],       label: "R" },
-      { param: "outerRadius", dir: [-1, 0, 0], pos: [-hw - o, -hh, 0],      label: "R" },
-      { param: "outerRadius", dir: [0,  0, 1], pos: [0, -hh, hd + o],       label: "R" },
-      { param: "innerRadius", dir: [-1, 0, 0], pos: [-innerR - o, -hh, 0],  label: "Ri" },
-      { param: "height",      dir: [0,  1, 0], pos: [0, hh + o, 0],         label: "H" },
+      { param: "outerRadius", dir: [1,  0, 0], pos: [hw + o,      -hh, 0],       label: "R"  },
+      { param: "outerRadius", dir: [-1, 0, 0], pos: [-(hw + o),   -hh, 0],       label: "R"  },
+      { param: "outerRadius", dir: [0,  0, 1], pos: [0,           -hh, hd + o],  label: "R"  },
+      { param: "innerRadius", dir: [0, -1, 0], pos: [0, -(hh + o), 0],            label: "Ri" },
+      { param: "height",      dir: [0,  1, 0], pos: [0,       hh + o,  0],       label: "H"  },
     ];
   }
 
